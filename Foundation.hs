a0facf3a7ab161f47191587560b339290745778f
Tim Baumann <tim@timbaumann.info>
2012-02-12 19:54:40 +0100

    Authentication with Email

--- Foundation.hs
+++ Foundation.hs
@@ -13,12 +13,14 @@ module Foundation
   ) where
 
 import Prelude
+import Control.Monad (liftM)
 import Yesod
 import Yesod.Static
 import Settings.StaticFiles
 import Yesod.Auth
 import Yesod.Auth.BrowserId
 import Yesod.Auth.GoogleEmail
+import Yesod.Auth.Email
 import Yesod.Default.Config
 import Yesod.Default.Util (addStaticContentExternal)
 import Yesod.Logger (Logger, logMsg, formatLogText)
@@ -34,11 +36,13 @@ import Settings (widgetFile, Extra (..))
 import Model
 import Text.Jasmine (minifym)
 import Web.ClientSession (getKey)
-import Text.Hamlet (hamletFile)
+import Text.Hamlet (hamletFile, shamlet)
+import Text.Blaze.Renderer.Text (renderHtml)
 #if DEVELOPMENT
 import qualified Data.Text.Lazy.Encoding
+import Network.Mail.Mime (renderMail', simpleMail, Mail, Address(..))
 #else
-import Network.Mail.Mime (sendmail)
+import Network.Mail.Mime (sendmail, renderMail', simpleMail, Mail, Address(..))
 #endif
 
 data Substantial = Substantial
@@ -66,6 +70,7 @@ instance Yesod Substantial where
   defaultLayout widget = do
     master <- getYesod
     mmsg <- getMessage
+    mauth <- maybeAuth
 
     pc <- widgetToPageContent $ do
       addStylesheet $ StaticR bootstrap_css_bootstrap_css
@@ -113,10 +118,85 @@ instance YesodAuth Substantial where
       Nothing -> do
         fmap Just $ insert $ User (credsIdent creds) Nothing
 
-  -- You can add other plugins like BrowserID, email or OAuth here
-  authPlugins _ = [authBrowserId, authGoogleEmail]
+  authPlugins _ = [authBrowserId, authGoogleEmail, authEmail]
 
   authHttpManager = httpManager
+
+instance YesodAuthEmail Substantial where
+  type AuthEmailId Substantial = EmailId
+
+  addUnverified email verkey = do
+    runDB $ insert $ Email email Nothing (Just verkey)
+  
+  sendVerifyEmail email _ verurl = do
+    y <- getYesod
+    mail <- liftIO $ simpleMail to emailFrom subject plain html []
+    liftIO $ renderSendMail y mail
+    where
+      to = Address Nothing email
+      subject = "Verify your email address"
+      plain = "" -- TODO
+      html = renderHtml [shamlet|
+<p>Activate your Substantial account by clicking on the link below.
+<p>
+  <a href="#{verurl}">#{verurl}
+|]
+
+  getVerifyKey eid = runDB $ do
+    liftM (maybe Nothing emailVerkey) $ get eid
+  
+  setVerifyKey eid verkey = runDB $ do
+    update eid [EmailVerkey =. Just verkey]
+
+  verifyAccount eid = runDB $ do
+    me <- get eid
+    case me of
+      Nothing -> return Nothing
+      Just e -> do
+        uid <- insert $ User (emailEmail e) Nothing
+        update eid [EmailUser =. Just uid]
+        return $ Just uid
+  
+  getPassword uid = runDB $ do
+    liftM (maybe Nothing userPassword) $ get uid
+
+  setPassword uid password = runDB $ do
+    update uid [UserPassword =. Just password]
+
+  getEmailCreds email = runDB $ do
+     me <- getBy $ UniqueEmail email
+     case me of
+       Nothing -> return Nothing
+       Just (Entity eid e) -> do
+         case emailUser e of
+           Nothing -> do
+             return $ Just $ EmailCreds
+               { emailCredsId = eid
+               , emailCredsAuthId = Nothing
+               , emailCredsStatus = False
+               , emailCredsVerkey = emailVerkey e
+               }
+           Just uid -> do
+             mu <- get uid
+             return $ Just $ EmailCreds
+               { emailCredsId = eid
+               , emailCredsAuthId = Just uid
+               , emailCredsStatus = case mu of
+                   Just (User { userPassword = Just _ }) -> True
+                   _ -> False
+               , emailCredsVerkey = emailVerkey e
+               }
+
+  getEmail eid = runDB $ do
+    liftM (fmap emailEmail) $ get eid
+
+    
+
+emailFrom :: Address
+emailFrom = Address Nothing "noreply@substantial.io"
+
+renderSendMail :: Substantial -> Mail -> IO ()
+renderSendMail s m = renderMail' m >>= deliver s
 
 -- Sends off your mail. Requires sendmail in production!
 deliver :: Substantial -> L.ByteString -> IO ()
