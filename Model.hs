97505eaf5d347cb14f771d22fb4170983ab0d441
Tim Baumann <tim@timbaumann.info>
2012-02-13 18:41:42 +0100

    Document model

--- Model.hs
+++ Model.hs
@@ -5,5 +5,15 @@ import Yesod
 import Data.Text (Text)
 import Database.Persist.Quasi
 
+data PublishSettings = Public
+                     | PublishedVersionsOnly
+                     | Private
+  deriving (Show, Read, Eq, Enum, Bounded)
+derivePersistField "PublishSettings"
+
+data Role = Author | Collaborator | Reviewer
+  deriving (Show, Read, Eq, Enum, Bounded)
+derivePersistField "Role"
+
 share [mkPersist sqlSettings, mkMigrate "migrateAll"]
   $(persistFileWith lowerCaseSettings "config/models")
