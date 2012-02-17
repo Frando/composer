6fecb90a41dc6eb2b11607521eb0d3dce8627afc
Tim Baumann <tim@timbaumann.info>
2012-02-17 14:53:55 +0100

    Extend Transaction Model

--- Model/Transaction.hs
+++ Model/Transaction.hs
@@ -4,37 +4,77 @@ module Model.Transaction
   ) where
 
 import Prelude
---import qualified Data.Text as T
+import qualified Data.Text as T
 import qualified Data.Vector as V
 import Data.Aeson
 import Control.Applicative ((<$>))
 import Control.Monad (mzero)
 
-data Annotation = Annotation deriving (Eq, Show)
+data Annotation = Annotation
+  { annotationType :: T.Text
+  } deriving (Eq, Show)
 
-data VEChar = VEChar Char [Annotation] deriving (Eq, Show)
+data Element = Paragraph
+             deriving (Eq, Show)
+
+data VEChar = VEChar Char [Annotation]
+            | StartTag Element
+            | EndTag Element
+            deriving (Eq, Show)
 type VEText = [VEChar]
 
 data Operation = Retain Int
                | Insert VEText
                | Delete VEText
+               | StartAnnotation Annotation
+               | StopAnnotation Annotation
                deriving (Eq, Show)
 
 data Transaction = Transaction Int [Operation] deriving (Eq, Show)
 
 instance ToJSON Annotation where
-  toJSON Annotation = Null
+  toJSON (Annotation type') = object
+    [ "type" .= type'
+    ]
 
 instance FromJSON Annotation where
-  parseJSON _ = return Annotation
+  parseJSON (Object o) = Annotation <$> o .: "type"
+  parseJSON _ = mzero
+
+elementToText :: Element -> T.Text
+elementToText Paragraph = "paragraph"
+
+elementFromText :: T.Text -> Maybe Element
+elementFromText "paragraph" = Just Paragraph
+elementFromText _ = Nothing
 
 instance ToJSON VEChar where
   toJSON (VEChar ch []) = toJSON ch
   toJSON (VEChar ch as) = toJSON $ (toJSON ch):(map toJSON as)
+  toJSON (StartTag e) = object
+    [ "type" .= elementToText e
+    ]
+  toJSON (EndTag e) = object
+    [ "type" .= T.cons '/' (elementToText e)
+    ]
 
 instance FromJSON VEChar where
-  parseJSON (Array a) | not (V.null a) = parseJSON $ V.head a
-  parseJSON s = (\ch -> VEChar ch []) <$> parseJSON s
+  parseJSON (Array a) | not (V.null a) = do
+    ch <- parseJSON $ V.head a
+    as <- parseJSON $ Array (V.tail a)
+    return $ VEChar ch as
+  parseJSON (String s) = (\ch -> VEChar ch []) <$> parseJSON (String s)
+  parseJSON (Object o) = do
+    type' <- o .: "type"
+    if T.null type'
+      then mzero
+      else do
+        let isEndTag = T.head type' == '/'
+        let elementText = if isEndTag then T.tail type' else type'
+        case elementFromText elementText of
+          Nothing -> mzero
+          Just e  -> return $ (if isEndTag then StartTag else EndTag) e
+  parseJSON _ = mzero
 
 instance ToJSON Operation where
   toJSON (Retain n) = object
@@ -49,6 +89,16 @@ instance ToJSON Operation where
     [ "type" .= String "remove"
     , "data" .= text
     ]
+  toJSON (StartAnnotation a) = object
+    [ "type" .= String "annotate"
+    , "bias" .= String "start"
+    , "annotation" .= a
+    ]
+  toJSON (StopAnnotation a) = object
+    [ "type" .= String "annotate"
+    , "bias" .= String "stop"
+    , "annotation" .= a
+    ]
 
 instance FromJSON Operation where
   parseJSON (Object o) = do
@@ -57,6 +107,13 @@ instance FromJSON Operation where
       String "retain" -> Retain <$> o .: "length"
       String "insert" -> Insert <$> o .: "data"
       String "remove" -> Delete <$> o .: "data"
+      String "annotate" -> do
+        bias <- o .: "bias"
+        annotation <- o .: "annotation"
+        case bias :: T.Text of
+          "start" -> return $ StartAnnotation annotation
+          "stop"  -> return $ StopAnnotation  annotation
+          _       -> mzero
       _ -> mzero
   parseJSON _ = mzero
 
