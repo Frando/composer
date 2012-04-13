950c3a66813c755cea5e7eb7a0c64b19da27b2ac
Michael Aufreiter <ma@zive.at>
2012-04-13 00:20:35 -0400

    Add util file.

---
+++ src/client/util.js
@@ -0,0 +1,19 @@
+// Helpers
+// ---------------
+
+s.util = {};
+
+// A fake console to calm down some browsers.
+if (!window.console) {
+  window.console = {
+    log: function(msg) {
+      // No-op
+    }
+  };
+}
+
+// Render Underscore templates
+_.tpl = function (tpl, ctx) {
+  var source = templates[tpl];
+  return _.template(source, ctx);
+};
